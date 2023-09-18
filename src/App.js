ce382b9bbb6056ce7230a3627212e6b2836e34d3
kiwonbyun <bkw9603@naver.com>
2023-09-18 09:34:41 +0900

    popover test

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
-import { useState } from 'react';
 import './App.css';
+import SummaryForm from './pages/summary/SummaryForm';
 
 export function replaceCamelWithSpace(colorName) {
   return colorName.replace(/\B([A-Z])\B/g, ' $1');
 }
 
 function App() {
-  const [color, setColor] = useState('MediumVioletRed');
-  const [disable, setDisable] = useState(false);
-  const newColor =
-    color === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
-  const handleClick = () => {
-    setColor(newColor);
-  };
-
-  const handleClickCheck = (e) => {
-    setDisable(e.target.checked);
-  };
-
   return (
     <div>
-      <input
-        id="disable-controller"
-        type="checkbox"
-        onChange={handleClickCheck}
-      />
-      <label htmlFor="disable-controller">Disable button</label>
-      <button
-        style={{ backgroundColor: disable ? 'gray' : color }}
-        onClick={handleClick}
-        disabled={disable}
-      >
-        Change to {newColor === 'MediumVioletRed' ? 'red' : 'blue'}
-      </button>
+      <SummaryForm />
     </div>
   );
 }
